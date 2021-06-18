ce0185e0692b956f0b155bd55b56aadcf973a958
alex <alxdev94@gmail.com>
2021-06-18 23:29:56 +0100

    testing config vars

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -11,6 +11,7 @@ import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.html.*
+import java.util.*
 
 fun HTML.index() {
     head {
@@ -38,7 +39,20 @@ fun main() {
                 call.respond("I'm alive!")
             }
             get("hello") {
-                call.respond(HttpStatusCode.Accepted, "Hello")
+
+                val env = System.getenv("DATABASE_URL");
+                val props = Properties()
+                val env2 = props.getProperty("DATABASE_URL")
+//
+//                println("================:${env}:")
+//                println("================:${props.getProperty("myprop")}:")
+//
+//                prop.stringPropertyNames()
+//                    .associateWith {prop.getProperty(it)}
+//                    .forEach { println(it) }
+
+
+                call.respond(HttpStatusCode.Accepted, "Hello + $env + $env2")
             }
             get("random/{min}/{max}") {
                 val min = call.parameters["min"]?.toIntOrNull() ?: 0
