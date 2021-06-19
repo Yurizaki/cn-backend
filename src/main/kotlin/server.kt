69109688bbd65921cf225d311b1bbf53d5642b22
alex <alxdev94@gmail.com>
2021-06-19 01:24:53 +0100

    db integration and GET json data on /hello

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -3,30 +3,103 @@ package main
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.*
-import io.ktor.html.respondHtml
 import io.ktor.http.HttpStatusCode
 import io.ktor.response.*
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import kotlinx.html.*
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
 import java.util.*
 
-fun HTML.index() {
-    head {
-        title("Hello from Ktor!")
-    }
-    body {
-        div {
-            +"Hello from Ktor"
-        }
-    }
+var db: Database? = null
+
+object cn_Vocabulary : Table() {
+    val cn_id = integer("cn_Id").autoIncrement()
+    val cn_character = varchar("cn_character", 10)
+    val cn_pinyin = varchar("cn_pinyin", 60)
+    val cn_translation = varchar("cn_translation", 100)
+    val cn_tags = varchar("cn_tags", 100).nullable()
+    val cn_related = varchar("cn_related", 100).nullable()
+    val cn_hskLevel = integer("cn_hskLevel")
+
+    override val primaryKey = PrimaryKey(cn_id, name = "PK_Vocabulary_ID")
 }
+
+fun dbStartup() {
+    val host = System.getenv("HEROKU_POSTGRESQL_KOTLIN_HOST")
+    val user = System.getenv("HEROKU_POSTGRESQL_KOTLIN_USER")
+    val pass = System.getenv("HEROKU_POSTGRESQL_KOTLIN_PASS")
+
+    if(host != null) {
+        db = Database.connect("jdbc:$host?sslmode=require", "org.postgresql.Driver", user, pass);
+
+        //    val db2 = Database.connect("jdbc:pgsql://ec2-52-7-115-250.compute-1.amazonaws.com:5432/d9fdbflj7g1t8a?ssl.mode=Require",
+//        driver = "com.impossibl.postgres.jdbc.PGDriver",
+//        user = "iebyhqwbwatafi",
+//        password = "7ab90e0fc97e192d4feca6f74c741acfa43d8cba33c685f031a984d75036a6f0")
+
+        transaction {
+
+            SchemaUtils.create(cn_Vocabulary)
+
+            val love = cn_Vocabulary.insert {
+                it[cn_character] = "爱"
+                it[cn_pinyin] = "ai"
+                it[cn_translation] = "to love"
+                it[cn_hskLevel] = 1
+            } get cn_Vocabulary.cn_id
+
+            val happy = cn_Vocabulary.insert {
+                it[cn_character] = "高兴"
+                it[cn_pinyin] = "gao xing"
+                it[cn_translation] = "happy"
+                it[cn_hskLevel] = 1
+            } get cn_Vocabulary.cn_id
+
+
+            for (city in cn_Vocabulary.selectAll()) {
+                println(city.toString())
+                println("$city")
+
+                test = city[cn_Vocabulary.cn_character]
+                test2 = city[cn_Vocabulary.cn_pinyin]
+                test3 = city[cn_Vocabulary.cn_translation]
+
+                val numbers = null
+
+
+                myNewList.add(
+                    mutableMapOf (
+                        "id" to "${city[cn_Vocabulary.cn_id]}",
+                        "character" to city[cn_Vocabulary.cn_character],
+                        "pinyin" to city[cn_Vocabulary.cn_pinyin],
+                        "translation" to city[cn_Vocabulary.cn_translation],
+                    )
+                )
+            }
+
+            SchemaUtils.drop (cn_Vocabulary)
+        }
+
+    }
+
+
+}
+
+var test = ""
+var test2: String? = null
+var test3: String? = null
+var myMap: Map<String, String>? = null
+
+var myNewList: MutableList<MutableMap<String, String>> = mutableListOf()
 
 fun main() {
     val port = System.getenv("PORT")?.toInt() ?: 23567
     println(port)
+
+    dbStartup()
 
     embeddedServer(Netty, port) {
         install(ContentNegotiation) {
@@ -51,8 +124,9 @@ fun main() {
 //                    .associateWith {prop.getProperty(it)}
 //                    .forEach { println(it) }
 
+                  call.respond(mapOf("vocabulary" to myNewList))
 
-                call.respond(HttpStatusCode.Accepted, "Hello + $env + $env2")
+//                call.respond(HttpStatusCode.Accepted, "Hello + $env + $env2 + $test")
             }
             get("random/{min}/{max}") {
                 val min = call.parameters["min"]?.toIntOrNull() ?: 0
